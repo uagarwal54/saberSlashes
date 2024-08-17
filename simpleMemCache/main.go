1a39f982a796a7d5d06e4a841d7a815cf1d07207
Udbhav Agarwal <uagarwal54@gmail.com>
2024-08-17 09:46:45 +0530

    Added the code to check if the store dir is not present then to create it

--- simpleMemCache/main.go
+++ simpleMemCache/main.go
@@ -4,12 +4,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	// keyValStore is initialized in common.go
 	keyValStore.keyValStore = make(map[string]string)
 	fmt.Println("Creating http router")
+	if _, err := os.Stat("./store"); os.IsNotExist(err) {
+		if err = os.Mkdir("./store", 0750); err != nil {
+			log.Fatal("Unable to create store dir: ", err)
+		}
+	}
 	router := http.NewServeMux()
 
 	fmt.Println("Registering Routes")
